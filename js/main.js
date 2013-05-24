339eeb0386c1f4e19061e40ed84ea517f02c9a86
Kyle Krafka <kyle.krafka@gmail.com>
2013-05-23 22:46:54 -0400

    Add different pages, keyboard interactivity, logo image

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+$(document).ready(function() {
+    // Save the original background color
+    var originalBodyBackgroundColor = $('body').css('backgroundColor');
 
+    // Start off by hiding everything but the welcome text
+    $('#game').hide();
+    $('#thanks').hide();
+
+    // Set up navigation buttons
+    $('#getstarted').click(function() {
+        $('#welcome').hide();
+        $('#game').show();
+    });
+    $('#quit').click(function() {
+        $('#game').hide();
+        $('#thanks').show();
+    });
+
+    // Set up keyboard commands
+    $(window).keypress(function(e) {
+        var key = e.which;
+        if (key == 32) { // space bar
+            $('#currentimage').css('borderColor', '#0f0');
+        } else if (key == 108) { // l key
+            // #666 == rgb(102, 102, 102)
+            // #333 == rgb(51, 51, 51)
+            var alternateColor = 'rgb(51, 51, 51)';
+            var oldColor = $('body').css('backgroundColor');
+            if(oldColor != alternateColor) {
+                $('body').css('backgroundColor', alternateColor);
+            } else {
+                $('body').css('backgroundColor', originalBodyBackgroundColor);
+            }
+        }
+    });
+
+    /*
+    To change the image:
+    $('#currentimage').attr('src', 'img/logo.png');
+    */
+});
