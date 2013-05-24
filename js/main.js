5ea32c025b6f5201bfbf775a1586016668ce03e8
Kyle Krafka <kyle.krafka@gmail.com>
2013-05-24 16:01:13 -0400

    Add front-end game logic and test images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// Game state
+var gameInProgress = false;
+
+
 $(document).ready(function() {
     // Save the original background color
     var originalBodyBackgroundColor = $('body').css('backgroundColor');
@@ -5,6 +9,10 @@ $(document).ready(function() {
     // Start off by hiding everything but the welcome text
     $('#game').hide();
     $('#thanks').hide();
+
+    // Hide game components by default
+    $('#currentimage').hide();
+    $('#seen').hide();
 
     // Set up navigation buttons
     $('#getstarted').click(function() {
@@ -32,22 +40,29 @@ $(document).ready(function() {
     $(window).keypress(function(e) {
         var key = e.which;
         if (key == 32) { // space bar
-            $('#currentimage').css('borderColor', '#0f0');
-        } else if (key == 108) { // l key
-            // #666 == rgb(102, 102, 102)
-            // #333 == rgb(51, 51, 51)
-            var alternateColor = 'rgb(51, 51, 51)';
-            var oldColor = $('body').css('backgroundColor');
-            if(oldColor != alternateColor) {
-                $('body').css('backgroundColor', alternateColor);
+            e.preventDefault();
+            if(!gameInProgress) {
+                playGame(['img/test1.jpg', 'img/test2.jpg', 'img/test3.jpg', 'img/test4.jpg', 'img/test5.jpg', 'img/test6.jpg'], 1000, 1400);
             } else {
-                $('body').css('backgroundColor', originalBodyBackgroundColor);
+                $('#seen').show();
             }
         }
     });
-
-    /*
-    To change the image:
-    $('#currentimage').attr('src', 'img/logo.png');
-    */
 });
+
+function playGame(imageURLs, displayTime, gapTime) {
+    $('#currentimage').queue(function() { gameInProgress = true; $('#beforelevel').hide(); $.dequeue(this); });
+    // Display all of the imagess
+    $.each(imageURLs, function(index, value) {
+        $('#currentimage')
+            .queue(function() { $(this).attr('src', value); $.dequeue(this); })
+            .show(0)
+            .delay(displayTime)
+            .hide(0)
+            .queue(function() { $(this).attr('src', ''); $.dequeue(this); }) // not really necessary
+            .delay(gapTime)
+            .queue(function() { $('#seen').hide(); $.dequeue(this); });
+    });
+    $('#currentimage').queue(function() { gameInProgress = false; $('#beforelevel').show(); $.dequeue(this); });
+
+}
